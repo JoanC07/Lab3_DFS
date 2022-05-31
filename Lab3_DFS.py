3e4f67a4c672bfea68a0f0cf3d1fc9336e0bd9ac
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-05-30 19:50:24 -0500

    Comentarios de parametros de nuestra clase principal

--- Lab3_DFS.py
+++ Lab3_DFS.py
@@ -19,12 +19,33 @@ class Grafo:
             Representación gráfica - Lista de adyacencia.
     Establecimiento de los parametros para el metodo constructor (inicializado)
 
-
+    Métodos
+    ------- 
+        Agregar_borde(self, nodo1, nodo2, peso=1):
+            Agregar arista al grafo.
+        Imprimir_lista_adyacencia(self):
+            Imprime la representacion grafica
+        __init__(self, numero_nodos, dirigido=True):
+            Recibe el numero de nodos y rango y verifica si es dirigido o no
+        def dfs(self, nodo_de_inicio, objetivo, camino = [], visitado = set()):
+            Imprimir el recorrido DFS de un vértice fuente dado.
 
     '''
 
     def __init__(self, numero_nodos, dirigido = True):
+        '''
+        Recibe el numero de nodos de nuestra clase principal Grafos. 
 
+        Parametros.
+            m_numero_nodos : int
+                 Numero de nodos 
+            numero_nodos : int
+                Rango de nodos 
+            m_dirigido : boolean
+                Tipo de grafo si es dirigida o no dirigida.
+            m_lista_adyacencia : diccionario
+                Representación gráfica - Lista de adyacencia.
+        '''
         # Numero de nodos
         self.m_numero_nodos = numero_nodos
         # Rango de nodos
